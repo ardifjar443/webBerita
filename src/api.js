2fad1fa36e3db678ca2e01d10c36b889fdd0c5cf
ardi <ardifjar443@gmail.com>
2023-10-16 18:36:35 +0700

    update: api

--- src/api.js
+++ src/api.js
@@ -6,35 +6,66 @@ let pesan = "berhasil";
 
 let endPoint = "/top-headlines?country=us";
 export const getBerita = async () => {
-  try {
-    const berita = await axios.get(
-      `${import.meta.env.VITE_BASE_URL}/api/berita`
-    );
-    if (berita.data.length === 0) {
-      return { type: "json", data: "tidak ada data" };
-    }
-    if (berita.headers["content-type"].includes("text/html")) {
-      loading = false;
-      return { type: "html", data: berita.data };
-    } else if (berita.headers["content-type"].includes("application/json")) {
-      loading = false;
-      return { type: "json", data: berita.data };
-    } else if (
-      berita.headers["content-type"].includes("text/plain; charset=utf-8")
-    ) {
-      if (berita.data === "") {
+  let endPoint;
+  if (import.meta.env.MODE === "development") {
+    try {
+      const berita = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/api/berita`
+      );
+      if (berita.data.length === 0) {
         return { type: "json", data: "tidak ada data" };
       }
-      loading = false;
-      return { type: "json", data: berita.data };
-    } else {
-      return "tidak ada data";
-    }
-  } catch (error) {
-    // Tangani kesalahan di sini
-    console.log(error);
+      if (berita.headers["content-type"].includes("text/html")) {
+        loading = false;
+        return { type: "html", data: berita.data };
+      } else if (berita.headers["content-type"].includes("application/json")) {
+        loading = false;
+        return { type: "json", data: berita.data };
+      } else if (
+        berita.headers["content-type"].includes("text/plain; charset=utf-8")
+      ) {
+        if (berita.data === "") {
+          return { type: "json", data: "tidak ada data" };
+        }
+        loading = false;
+        return { type: "json", data: berita.data };
+      } else {
+        return "tidak ada data";
+      }
+    } catch (error) {
+      // Tangani kesalahan di sini
+      console.log(error);
 
-    return error; // Atau Anda dapat mengembalikan nilai atau status kesalahan khusus
+      return error; // Atau Anda dapat mengembalikan nilai atau status kesalahan khusus
+    }
+  } else {
+    try {
+      const berita = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/data/data.json`
+      );
+
+      if (berita.headers["content-type"].includes("text/html")) {
+        loading = false;
+        return { type: "html", data: berita.data };
+      } else if (berita.headers["content-type"].includes("application/json")) {
+        loading = false;
+        return { type: "json", data: berita.data };
+      } else if (
+        berita.headers["content-type"].includes("text/plain; charset=utf-8")
+      ) {
+        if (berita.data === "") {
+          return { type: "json", data: "tidak ada data" };
+        }
+        loading = false;
+        return { type: "json", data: berita.data };
+      } else {
+        return "tidak ada data";
+      }
+    } catch (error) {
+      // Tangani kesalahan di sini
+      console.log(error);
+      return error; // Atau Anda dapat mengembalikan nilai atau status kesalahan khusus
+    }
   }
 };
 export const setSearch = (id) => {
@@ -50,19 +81,31 @@ export const getUploadLoading = () => {
 };
 
 export const article = async (id) => {
-  console.log(id);
-  // console.log("article: " + endPoint);
-  const berita = await axios.get(
-    `${import.meta.env.VITE_BASE_URL}/api/berita/${id}`
-  );
+  if (import.meta.env.MODE === "development") {
+    // console.log("article: " + endPoint);
+    const berita = await axios.get(
+      `${import.meta.env.VITE_BASE_URL}/api/berita/${id}`
+    );
 
-  loading = false;
-  // if (berita.data.filter((item) => item.id.includes(id))[0] === undefined) {
-  //   return "tidak ada article";
-  // }
+    loading = false;
+    // if (berita.data.filter((item) => item.id.includes(id))[0] === undefined) {
+    //   return "tidak ada article";
+    // }
 
-  // return berita.data.filter((item) => item.id.includes(id))[0];
-  return berita.data;
+    // return berita.data.filter((item) => item.id.includes(id))[0];
+    return berita.data;
+  } else {
+    const berita = await axios.get(
+      `${import.meta.env.VITE_BASE_URL}/data/data.json`
+    );
+
+    loading = false;
+    if (berita.data.filter((item) => item.id.includes(id))[0] === undefined) {
+      return "tidak ada article";
+    }
+
+    return berita.data.filter((item) => item.id.includes(id))[0];
+  }
 };
 
 export const setNegara = (id) => {
